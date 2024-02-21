42d0a48b6f4276fe149e7344b729d3b4ae6aa20d
tanushreebanerjee <tb21@princeton.edu>
2024-02-21 06:12:09 -0500

    use autoencoder inherited from LDM repo

--- src/models/autoencoder.py
+++ src/models/autoencoder.py
@@ -3,5 +3,6 @@
 from ldm.models.autoencoder import AutoencoderKL
 
 class Autoencoder(AutoencoderKL):
+    """Autoencoder model with KL divergence loss."""
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
