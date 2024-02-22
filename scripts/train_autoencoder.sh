e8761ff83c9f2e1f5fb2ea92afd251f232f946c0
tanushreebanerjee <tb21@princeton.edu>
2024-02-22 14:32:49 -0500

    commit

---
+++ scripts/train_autoencoder.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+source ~/.bashrc && conda activate gen-detection
+
+GPUS=0
+export CUDA_VISIBLE_DEVICES=$GPUS
+
+MODE=train
+MODEL=autoencoder
+DATASET=shapenet
+SCRIPT_NAME=${MODE}_${MODEL}.py
+
+CONFIG_DIR=configs
+CONFIG_SPEC=${MODEL}_kl_8x8x64
+
+echo "Running $SCRIPT_NAME with $CONFIG_DIR/$MODEL/$MODE\_$DATASET/$CONFIG_SPEC.yaml"
+python $SCRIPT_NAME --base $CONFIG_DIR/$MODEL/$MODE\_$DATASET/$CONFIG_SPEC.yaml -t --gpus $GPUS
