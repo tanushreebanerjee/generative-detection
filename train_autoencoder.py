ddfb731128f5f532299f45fb201c0d04f170c179
tanushreebanerjee <tb21@princeton.edu>
2024-02-22 15:32:07 -0500

    commit

--- train_autoencoder.py
+++ train_autoencoder.py
@@ -13,6 +13,7 @@ set_submodule_paths(submodule_dir="submodules")
 from ldm.util import instantiate_from_config
 
 def get_parser(**parser_kwargs):
+    """Get parser for command line arguments."""
     def str2bool(v):
         if isinstance(v, bool):
             return v
@@ -22,111 +23,20 @@ def get_parser(**parser_kwargs):
             return False
         else:
             raise argparse.ArgumentTypeError("Boolean value expected.")
-
     parser = argparse.ArgumentParser(**parser_kwargs)
-    
-    # cache directories
-    parser.add_argument(
-        "--transformers_cache",
-        type=str,
-        default=".cache/transformers_cache",
-        help="transformers cache directory",
-    )
-    
-    parser.add_argument(
-        "--torch_home",
-        type=str,
-        default=".cache/torch_home",
-        help="torch home directory",
-    )
-    
-    parser.add_argument(
-        "-n",
-        "--name",
-        type=str,
-        const=True,
-        default="",
-        nargs="?",
-        help="postfix for logdir",
-    )
-    parser.add_argument(
-        "-r",
-        "--resume",
-        type=str,
-        const=True,
-        default="",
-        nargs="?",
-        help="resume from logdir or checkpoint in logdir",
-    )
-    parser.add_argument(
-        "-b",
-        "--base",
-        nargs="*",
-        metavar="base_config.yaml",
-        help="paths to base configs. Loaded from left-to-right. "
-             "Parameters can be overwritten or added with command-line options of the form `--key value`.",
-        default=list(),
-    )
-    parser.add_argument(
-        "-t",
-        "--train",
-        type=str2bool,
-        const=True,
-        default=False,
-        nargs="?",
-        help="train",
-    )
-    parser.add_argument(
-        "--no-test",
-        type=str2bool,
-        const=True,
-        default=False,
-        nargs="?",
-        help="disable test",
-    )
-    parser.add_argument(
-        "-p",
-        "--project",
-        help="name of new or path to existing project"
-    )
-    parser.add_argument(
-        "-d",
-        "--debug",
-        type=str2bool,
-        nargs="?",
-        const=True,
-        default=False,
-        help="enable post-mortem debugging",
-    )
-    parser.add_argument(
-        "-s",
-        "--seed",
-        type=int,
-        default=23,
-        help="seed for seed_everything",
-    )
-    parser.add_argument(
-        "-f",
-        "--postfix",
-        type=str,
-        default="",
-        help="post-postfix for default name",
-    )
-    parser.add_argument(
-        "-l",
-        "--logdir",
-        type=str,
-        default="logs",
-        help="directory for logging dat shit",
-    )
-    parser.add_argument(
-        "--scale_lr",
-        type=str2bool,
-        nargs="?",
-        const=True,
-        default=True,
-        help="scale base-lr by ngpu * batch_size * n_accumulate",
-    )
+    parser.add_argument("--transformers_cache",type=str,default=".cache/transformers_cache", help="transformers cache directory",)
+    parser.add_argument("--torch_home", type=str, default=".cache/torch_home", help="torch home directory")
+    parser.add_argument("-n", "--name", type=str, const=True, default="", nargs="?", help="postfix for logdir")
+    parser.add_argument("-r", "--resume", type=str, const=True, default="", nargs="?", help="resume from logdir or checkpoint in logdir")
+    parser.add_argument("-b", "--base", nargs="*", metavar="base_config.yaml", default=list(), help="paths to base configs. Loaded from left-to-right. Parameters can be overwritten or added with command-line options of the form `--key value`.")
+    parser.add_argument("-t", "--train", type=str2bool, const=True, default=False, nargs="?", help="train")
+    parser.add_argument("--no-test", type=str2bool, const=True, default=False, nargs="?", help="disable test")
+    parser.add_argument("-p", "--project", help="name of new or path to existing project")
+    parser.add_argument("-d", "--debug", type=str2bool, nargs="?", const=True, default=False, help="enable post-mortem debugging")
+    parser.add_argument("-s", "--seed", type=int, default=23, help="seed for seed_everything")
+    parser.add_argument("-f", "--postfix", type=str, default="", help="post-postfix for default name")
+    parser.add_argument("-l", "--logdir", type=str, default="logs", help="directory for logging dat shit")
+    parser.add_argument("--scale_lr", type=str2bool,nargs="?",const=True,default=True,help="scale base-lr by ngpu * batch_size * n_accumulate")
     return parser
 
 def nondefault_trainer_args(opt):
