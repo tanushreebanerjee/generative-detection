cef014c40794b1edb806e986e6727817d16307a2
tanushreebanerjee <tb21@princeton.edu>
2024-02-25 22:19:37 -0500

    commit

--- src/data/datasets/shapenet.py
+++ src/data/datasets/shapenet.py
@@ -2,10 +2,16 @@
 import os
 import json
 import numpy as np
-import numpy as np
+import albumentations
+from functools import partial
+import PIL
+from PIL import Image
+import torchvision.transforms.functional as TF
 from torch.utils.data import Dataset
 from omegaconf import OmegaConf
+import cv2
 from taming.data.imagenet import retrieve, ImagePaths
+from ldm.modules.image_degradation import degradation_fn_bsr, degradation_fn_bsr_light
 
 class ShapeNetBase(Dataset):
     def __init__(self, config=None):
@@ -168,3 +174,127 @@ class ShapeNetTest(ShapeNetBase):
     def _prepare(self):
         self.random_crop = retrieve(self.config, "random_crop", default=False)
     
+class ShapeNetSR(Dataset):
+    def __init__(self, size=None,
+                 degradation=None, downscale_f=4, min_crop_f=0.5, max_crop_f=1.,
+                 random_crop=True):
+        """
+        ShapeNet Super-Resolution Dataset
+        Performs following ops in order:
+        1.  crops a crop of size s from image either as random or center crop
+        2.  resizes crop to size with cv2.area_interpolation
+        3.  degrades resized crop with degradation_fn
+
+        :param size: resizing to size after cropping
+        :param degradation: degradation_fn, e.g. cv_bicubic or bsrgan_light
+        :param downscale_f: Low Resolution Downsample factor
+        :param min_crop_f: determines crop size s,
+          where s = c * min_img_side_len with c sampled from interval (min_crop_f, max_crop_f)
+        :param max_crop_f: ""
+        :param data_root:
+        :param random_crop:
+        """    
+        
+        self.base = self.get_base()
+        
+        assert size is not None, "size must be specified"
+        assert (size / downscale_f).is_integer(), "size must be divisible by downscale_f"
+        self.size = size
+        self.LR_size = int(size / downscale_f)
+        
+        self.min_crop_f = min_crop_f
+        self.max_crop_f = max_crop_f
+        
+        assert(max_crop_f <= 1.)
+        self.center_crop = not random_crop
+        
+        self.image_rescaler = albumentations.SmallestMaxSize(max_size=size, interpolation=cv2.INTER_AREA)
+        self.pil_interpolation = False # gets reset later if incase interp_op is from pillow
+        
+        if degradation == "bsrgan":
+            self.degradation_process = partial(degradation_fn_bsr, sf=downscale_f)
+
+        elif degradation == "bsrgan_light":
+            self.degradation_process = partial(degradation_fn_bsr_light, sf=downscale_f)
+
+        else:
+            interpolation_fn = {
+            "cv_nearest": cv2.INTER_NEAREST,
+            "cv_bilinear": cv2.INTER_LINEAR,
+            "cv_bicubic": cv2.INTER_CUBIC,
+            "cv_area": cv2.INTER_AREA,
+            "cv_lanczos": cv2.INTER_LANCZOS4,
+            "pil_nearest": PIL.Image.NEAREST,
+            "pil_bilinear": PIL.Image.BILINEAR,
+            "pil_bicubic": PIL.Image.BICUBIC,
+            "pil_box": PIL.Image.BOX,
+            "pil_hamming": PIL.Image.HAMMING,
+            "pil_lanczos": PIL.Image.LANCZOS,
+            }[degradation]
+
+            self.pil_interpolation = degradation.startswith("pil_")
+
+            if self.pil_interpolation:
+                self.degradation_process = partial(TF.resize, size=self.LR_size, interpolation=interpolation_fn)
+
+            else:
+                self.degradation_process = albumentations.SmallestMaxSize(max_size=self.LR_size,
+                                                                          interpolation=interpolation_fn)
+    def __len__(self):
+        return len(self.base)
+    
+    def __getitem__(self, i):
+        example = self.base[i]
+        image = Image.open(example["file_path_"])
+
+        if not image.mode == "RGB":
+            image = image.convert("RGB")
+
+        image = np.array(image).astype(np.uint8)
+
+        min_side_len = min(image.shape[:2])
+        crop_side_len = min_side_len * np.random.uniform(self.min_crop_f, self.max_crop_f, size=None)
+        crop_side_len = int(crop_side_len)
+
+        if self.center_crop:
+            self.cropper = albumentations.CenterCrop(height=crop_side_len, width=crop_side_len)
+
+        else:
+            self.cropper = albumentations.RandomCrop(height=crop_side_len, width=crop_side_len)
+
+        image = self.cropper(image=image)["image"]
+        image = self.image_rescaler(image=image)["image"]
+
+        if self.pil_interpolation:
+            image_pil = PIL.Image.fromarray(image)
+            LR_image = self.degradation_process(image_pil)
+            LR_image = np.array(LR_image).astype(np.uint8)
+
+        else:
+            LR_image = self.degradation_process(image=image)["image"]
+
+        example["image"] = (image/127.5 - 1.0).astype(np.float32)
+        example["LR_image"] = (LR_image/127.5 - 1.0).astype(np.float32)
+
+        return example
+        
+class ShapeNetSRTrain(ShapeNetSR):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        
+    def get_base(self):
+        return ShapeNetTrain()
+
+class ShapeNetSRValidation(ShapeNetSR):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        
+    def get_base(self):
+        return ShapeNetValidation()
+    
+class ShapeNetSRTest(ShapeNetSR):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        
+    def get_base(self):
+        return ShapeNetTest()
