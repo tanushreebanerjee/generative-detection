cd6cf9633dcdcfdf7a699cf50d9f0ed6d3127ea9
tanushreebanerjee <tb21@princeton.edu>
2024-02-28 14:47:31 -0500

    PoseAutoencoder updates

--- src/models/autoencoder.py
+++ src/models/autoencoder.py
@@ -10,19 +10,50 @@ class Autoencoder(AutoencoderKL):
         super().__init__(*args, **kwargs)
         
 class PoseAutoencoder(AutoencoderKL):
-    """Autoencoder model with KL divergence loss."""
+    """
+    Autoencoder model for pose encoding and decoding.
+    """
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.pose_encoder = PoseEncoder(*args, **kwargs)
         self.pose_decoder = PoseDecoder(*args, **kwargs)
         
     def decode_pose(self, x):
+        """
+        Decode the pose from the given input.
+        
+        Args:
+            x: Input tensor.
+        
+        Returns:
+            Decoded pose tensor.
+        """
         return self.pose_decoder(x)
     
     def encode_pose(self, x):
+        """
+        Encode the pose from the given input.
+        
+        Args:
+            x: Input tensor.
+        
+        Returns:
+            Encoded pose tensor.
+        """
         return self.pose_encoder(x)
     
     def forward(self, input, sample_posterior=True):
+        """
+        Forward pass of the autoencoder.
+        
+        Args:
+            input: Input tensor.
+            sample_posterior: Whether to sample from the posterior distribution.
+        
+        Returns:
+            Tuple containing the feature map with pose information and the decoded pose.
+        """
         posterior = self.encode(input)
         if sample_posterior:
             feat_map = posterior.sample()
@@ -35,4 +66,4 @@ class PoseAutoencoder(AutoencoderKL):
         pose_feat_map = self.decode(pose_encoded)
         
         feat_map_pose = feat_map + pose_feat_map
-        return feat_map_pose, posterior
+        return feat_map_pose, pose_decoded
