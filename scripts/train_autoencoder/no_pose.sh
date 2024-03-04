4375bd79fb0a5caf129a4cce1ff4e8999df2e16d
tanushreebanerjee <tb21@princeton.edu>
2024-03-03 22:36:21 -0500

    minor

--- scripts/train_autoencoder/no_pose.sh
+++ scripts/train_autoencoder/no_pose.sh
@@ -11,7 +11,8 @@ DATASET=shapenet
 SCRIPT_NAME=${MODE}_${MODEL}.py
 
 CONFIG_DIR=configs
-CONFIG_SPEC=${MODEL}_kl_8x8x64
+SPEC=16x16x16 # 8x8x64, 16x16x16, 32x32x4, 64x64x3
+CONFIG_SPEC=${MODEL}_kl_${SPEC}
 
 echo "python $SCRIPT_NAME --base $CONFIG_DIR/$MODEL/$MODE_$DATASET/no-pose/$CONFIG_SPEC.yaml -t --gpus $GPUS"
 python $SCRIPT_NAME --base $CONFIG_DIR/$MODEL/$MODE\_$DATASET/no-pose/$CONFIG_SPEC.yaml -t --gpus $GPUS
