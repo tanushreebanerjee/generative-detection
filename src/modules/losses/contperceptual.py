eea0399f1bfdf9a8a73ba054643551448fc084dd
tanushreebanerjee <tb21@princeton.edu>
2024-03-05 01:25:38 -0500

    loss update

--- src/modules/losses/contperceptual.py
+++ src/modules/losses/contperceptual.py
@@ -1,7 +1,8 @@
 # src/modules/losses/contperceptual.py
 import torch.nn as nn
 from ldm.modules.losses.contperceptual import LPIPSWithDiscriminator as LPIPSWithDiscriminator_LDM
-# from se3.homtrans3d import T2xyzrpy
+from taming.modules.losses.vqperceptual import adopt_weight
+import torch
 import math
 
 SE3_DIM = 16
@@ -26,23 +27,77 @@ class PoseLoss(LPIPSWithDiscriminator_LDM):
                 last_layer=None, cond=None, split="train",
                 weights=None):
         
-        loss, log \
-            = super().forward(inputs, reconstructions, posteriors, optimizer_idx,
-                global_step, last_layer, cond, split,
-                weights)
-        assert pose_inputs.shape == pose_reconstructions.shape
-        assert pose_inputs.shape[1] == int(math.sqrt(SE3_DIM)), pose_inputs.shape[2] == int(math.sqrt(SE3_DIM))
+        assert pose_inputs.shape == pose_reconstructions.shape, f"pose_inputs.shape: {pose_inputs.shape}, pose_reconstructions.shape: {pose_reconstructions.shape}"
+        assert pose_inputs.shape[1] == int(math.sqrt(SE3_DIM)), pose_inputs.shape[2] == int(math.sqrt(SE3_DIM)), f"pose_inputs.shape: {pose_inputs.shape}"
 
         pose_loss = self.compute_pose_loss(pose_inputs, pose_reconstructions)
         weighted_pose_loss = self.pose_weight * pose_loss
-        print("total loss without pose loss: ", loss)
         print("pose loss: ", pose_loss)
         print("weighted pose loss: ", weighted_pose_loss)
-        loss += weighted_pose_loss 
         
-        log["{}/pose_loss".format(split)] = pose_loss.clone().detach().mean()
-        log["{}/weighted_pose_loss".format(split)] = weighted_pose_loss.clone().detach().mean()
-        log["{}/total_loss".format(split)] = loss.clone().detach().mean()
-        
-        return loss, log
-            
+        rec_loss = torch.abs(inputs.contiguous() - reconstructions.contiguous())
+        if self.perceptual_weight > 0:
+            p_loss = self.perceptual_loss(inputs.contiguous(), reconstructions.contiguous())
+            rec_loss = rec_loss + self.perceptual_weight * p_loss
+
+        nll_loss = rec_loss / torch.exp(self.logvar) + self.logvar
+        weighted_nll_loss = nll_loss
+        if weights is not None:
+            weighted_nll_loss = weights*nll_loss
+        weighted_nll_loss = torch.sum(weighted_nll_loss) / weighted_nll_loss.shape[0]
+        nll_loss = torch.sum(nll_loss) / nll_loss.shape[0]
+        kl_loss = posteriors.kl()
+        kl_loss = torch.sum(kl_loss) / kl_loss.shape[0]
+
+        # now the GAN part
+        if optimizer_idx == 0:
+            # generator update
+            if cond is None:
+                assert not self.disc_conditional
+                logits_fake = self.discriminator(reconstructions.contiguous())
+            else:
+                assert self.disc_conditional
+                logits_fake = self.discriminator(torch.cat((reconstructions.contiguous(), cond), dim=1))
+            g_loss = -torch.mean(logits_fake)
+
+            if self.disc_factor > 0.0:
+                try:
+                    d_weight = self.calculate_adaptive_weight(nll_loss, g_loss, last_layer=last_layer)
+                except RuntimeError:
+                    assert not self.training
+                    d_weight = torch.tensor(0.0)
+            else:
+                d_weight = torch.tensor(0.0)
+
+            disc_factor = adopt_weight(self.disc_factor, global_step, threshold=self.discriminator_iter_start)
+            loss = weighted_pose_loss + weighted_nll_loss + self.kl_weight * kl_loss + d_weight * disc_factor * g_loss
+
+            log = {"{}/total_loss".format(split): loss.clone().detach().mean(), "{}/logvar".format(split): self.logvar.detach(),
+                   "{}/kl_loss".format(split): kl_loss.detach().mean(), "{}/nll_loss".format(split): nll_loss.detach().mean(),
+                   "{}/rec_loss".format(split): rec_loss.detach().mean(),
+                   "{}/d_weight".format(split): d_weight.detach(),
+                   "{}/disc_factor".format(split): torch.tensor(disc_factor),
+                   "{}/g_loss".format(split): g_loss.detach().mean(),
+                   "{}/pose_loss".format(split): pose_loss.detach().mean(),
+                   "{}/weighted_pose_loss".format(split): weighted_pose_loss.detach().mean()
+                   }
+            print("loss: ", loss)
+            return loss, log
+
+        if optimizer_idx == 1:
+            # second pass for discriminator update
+            if cond is None:
+                logits_real = self.discriminator(inputs.contiguous().detach())
+                logits_fake = self.discriminator(reconstructions.contiguous().detach())
+            else:
+                logits_real = self.discriminator(torch.cat((inputs.contiguous().detach(), cond), dim=1))
+                logits_fake = self.discriminator(torch.cat((reconstructions.contiguous().detach(), cond), dim=1))
+
+            disc_factor = adopt_weight(self.disc_factor, global_step, threshold=self.discriminator_iter_start)
+            d_loss = disc_factor * self.disc_loss(logits_real, logits_fake)
+
+            log = {"{}/disc_loss".format(split): d_loss.clone().detach().mean(),
+                   "{}/logits_real".format(split): logits_real.detach().mean(),
+                   "{}/logits_fake".format(split): logits_fake.detach().mean()
+                   }
+            return d_loss, log
