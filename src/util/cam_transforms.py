4fe2a1efea8f2d2be524e3c93a8a1d7ad7419102
tanushreebanerjee <tb21@princeton.edu>
2024-04-02 17:01:22 -0400

    commit

--- src/util/cam_transforms.py
+++ src/util/cam_transforms.py
@@ -28,7 +28,8 @@ def get_patch_ndc_proj_matrix(patch_size, image_size, patch_center):
     sx = 2 * patch_size[:, 1] / image_size[:, 1]
     sy = 2 * patch_size[:, 0] / image_size[:, 0]
     
-    cx = (2 * patch_center[:, 0] / image_size[:, 1]) - 1
+    # offset from center in x with origin at top left, center coords H/2, W/2
+    cx = (2 * patch_center[:, 0] / image_size[:, 1]) - 1 
     cy = (2 * patch_center[:, 1] / image_size[:, 0]) - 1
     
     batch_size = patch_size.shape[0]
@@ -82,11 +83,11 @@ def patch_ndc2_full_ndc(patch_size, image_size, patch_center):
     H_full, W_full = image_size[:, 0], image_size[:, 1]
     H_patch, W_patch = patch_size[:, 0], patch_size[:, 1]
     
-    cx_patch, cy_patch = patch_center[:, 0], patch_center[:, 1] # patch center in image coordinates
-    sx_patch, sy_patch = patch_ndc_proj_matrix[:, 0, 0], patch_ndc_proj_matrix[:, 1, 1] # patch scale in x and y
-    
-    sx_full = W_full / W_patch
-    sy_full = H_full / H_patch
+    cx_patch, cy_patch = patch_center[:, 0], patch_center[:, 1] # patch center in x and y in pixels, origin at top left
+    sx_patch, sy_patch = patch_ndc_proj_matrix[:, 0, 0], patch_ndc_proj_matrix[:, 1, 1] # patch scale in x and y from NDC to screen patch
+
+    sx_full = (W_patch / W_full) * sx_patch
+    sy_full = (H_patch / H_full) * sy_patch
     
     cx_full = (cx_patch * sx_patch) - (W_full / 2)
     cy_full = (cy_patch * sy_patch) - (H_full / 2)
@@ -95,6 +96,7 @@ def patch_ndc2_full_ndc(patch_size, image_size, patch_center):
     full_ndc_proj_matrix[:, 1, 1] = sy_full
     full_ndc_proj_matrix[:, 0, 2] = cx_full
     full_ndc_proj_matrix[:, 1, 2] = cy_full
+    
     return full_ndc_proj_matrix
  
 def full_ndc2_patch_ndc(patch_ndc_proj_matrix, patch_size, image_size, patch_center):
@@ -170,8 +172,6 @@ def camera_coords2_full_screen(camera_coords, K):
         torch.Tensor: The transform in screen coordinates. shape: (batch_size, 4, 4)
     """
     
-    K_inv = torch.inverse(K) # shape: b, 4, 4
-    
     camera2screen_transform = torch.eye(4).unsqueeze(0).repeat(camera_coords.shape[0], 1, 1)
     camera2screen_transform[:, :3, 3] = camera_coords
     
@@ -179,9 +179,16 @@ def camera_coords2_full_screen(camera_coords, K):
     
     return camera2screen_transform
 
-def get_screen_coords_from_patch(patch_size, image_size, patch_center, cameras, with_xyflip, K):
-    patch_ndc2full_ndc = patch_ndc2_full_ndc(patch_size, image_size, patch_center)
+def camera_coords_from_patch_NDC(patch_se3_pose, patch_size, image_size, patch_center,
+                                 cameras, with_xyflip=False):
     
+    patch_ndc2full_ndc = patch_ndc2_full_ndc(patch_size, image_size, patch_center)
     full_ndc2full_screen = full_ndc2full_screen(cameras, with_xyflip, image_size)
     
-    return torch.matmul(full_ndc2full_screen, patch_ndc2full_ndc)
+    patch_ndc2full_screen = torch.matmul(full_ndc2full_screen, patch_ndc2full_ndc)
+    
+    patch_screen2camera = torch.matmul(patch_se3_pose, patch_ndc2full_screen)
+    
+    camera_coords = full_screen2_camera_coords(patch_screen2camera, cameras.K)
+
+    return camera_coords
