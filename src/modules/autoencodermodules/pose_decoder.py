afe93f842fde3be7c2a28b0205fa77f3544001b0
tanushreebanerjee <tb21@princeton.edu>
2024-04-15 20:53:12 -0400

    pose enc dec MLP update

--- src/modules/autoencodermodules/pose_decoder.py
+++ src/modules/autoencodermodules/pose_decoder.py
@@ -1,12 +1,13 @@
 # src/modules/autoencodermodules/pose_decoder.py
 import torch.nn as nn
+import logging
 
 class PoseDecoder(nn.Module):
     """
     Decoder module for pose estimation.
     """
 
-    def __init__(self, enc_feat_dims, pose_feat_dims):
+    def __init__(self, enc_feat_dims, pose_feat_dims, activation="relu"):
         """
         Initialize the PoseDecoder module.
 
@@ -16,8 +17,29 @@ class PoseDecoder(nn.Module):
         """
         
         super(PoseDecoder, self).__init__()
-        self.fc = nn.Linear(enc_feat_dims, pose_feat_dims)
-    
+        
+        hidden_dim_1 = enc_feat_dims // 4
+        hidden_dim_2 = enc_feat_dims // 8
+        
+        if activation == "relu":
+            activation = nn.ReLU()
+        elif activation == "softplus":
+            activation = nn.Softplus()
+        else:
+            raise ValueError("Invalid activation function. Please provide a valid activation function in ['relu', 'softplus'].")
+        
+        self.fc = nn.Sequential(
+            nn.Linear(enc_feat_dims, hidden_dim_1),
+            activation,
+            nn.Linear(hidden_dim_1, hidden_dim_2),
+            activation,
+            nn.Linear(hidden_dim_2, pose_feat_dims)
+        )
+        
+        logging.info("PoseDecoder initialized.")
+        logging.info(self.fc)
+        
+        
     def forward(self, x):
         """
         Forward pass of the pose decoder.
--- src/modules/autoencodermodules/pose_encoder.py
+++ src/modules/autoencodermodules/pose_encoder.py
@@ -1,12 +1,13 @@
 # src/modules/autoencodermodules/pose_encoder.py
 import torch.nn as nn
+import logging
 
 class PoseEncoder(nn.Module): 
     """
     PoseEncoder is a module that encodes pose features into image features.
     """
 
-    def __init__(self, enc_feat_dims, pose_feat_dims):
+    def __init__(self, enc_feat_dims, pose_feat_dims, activation="relu"):
         """
         Initializes a new instance of the PoseEncoder class.
 
@@ -15,8 +16,27 @@ class PoseEncoder(nn.Module):
             pose_feat_dims (int): Dimensionality of the pose features.
         """
         super(PoseEncoder, self).__init__()
-        self.fc = nn.Linear(pose_feat_dims, enc_feat_dims)
-    
+        
+        hidden_dim_1 = enc_feat_dims // 8
+        hidden_dim_2 = enc_feat_dims // 4
+        
+        if activation == "relu":
+            activation = nn.ReLU()
+        elif activation == "softplus":
+            activation = nn.Softplus()
+        else:
+            raise ValueError("Invalid activation function. Please provide a valid activation function in ['relu', 'softplus'].")
+        
+        self.fc = nn.Sequential(
+            nn.Linear(pose_feat_dims, hidden_dim_1),
+            activation,
+            nn.Linear(hidden_dim_1, hidden_dim_2),
+            activation,
+            nn.Linear(hidden_dim_2, enc_feat_dims)
+        )
+        logging.info("PoseEncoder initialized.")
+        logging.info(self.fc)
+        
     def forward(self, x):
         """
         Forward pass of the PoseEncoder module.
