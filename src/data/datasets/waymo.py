e3eb19947918e18993c88c0d978aaa65f4cd8491
Ishaan Javali <ij9461@wash.cs.princeton.edu>
2024-04-30 14:53:59 -0400

    Changed labels and cameras

--- src/data/datasets/waymo.py
+++ src/data/datasets/waymo.py
@@ -19,22 +19,15 @@ import numpy as np
 
 # TODO: Are these labels below correct for Waymo
 LABEL_NAME2ID = {
-    'car': 0, 
-    'truck': 1,
-    'trailer': 2,
-    'bus': 3,
-    'construction_vehicle': 4,
-    'bicycle': 5,
-    'motorcycle': 6,
-    'pedestrian': 7,
-    'traffic_cone': 8,
-    'barrier': 9
+    'Car': 0, 
+    'Pedestrian': 1,
+    'Cyclist': 2
 }
 
 LABEL_ID2NAME = {v: k for k, v in LABEL_NAME2ID.items()}
 
 CAM_NAMESPACE = 'CAM'
-CAMERAS = ["FRONT", "FRONT_RIGHT", "FRONT_LEFT", "BACK", "BACK_LEFT", "BACK_RIGHT"]
+CAMERAS = ["FRONT", "FRONT_LEFT", "FRONT_RIGHT", "SIDE_LEFT", "SIDE_RIGHT"]
 CAMERA_NAMES = [f"{CAM_NAMESPACE}_{camera}" for camera in CAMERAS]
 CAM_NAME2CAM_ID = {cam_name: i for i, cam_name in enumerate(CAMERA_NAMES)}
 CAM_ID2CAM_NAME = {i: cam_name for i, cam_name in enumerate(CAMERA_NAMES)}
