6e1b01d0a23972884aa8ab7bfcef5186739a2adf
Ishaan Javali <ij9461@spin.cs.princeton.edu>
2024-05-02 17:19:43 -0400

    Code for testing waymo

--- test_waymo.py
+++ test_waymo.py
@@ -1,5 +1,6 @@
 from src.data.datasets.nuscenes import NuScenesValidationMini, LABEL_ID2NAME
 from src.data.datasets.waymo import WaymoValidationMini, LABEL_ID2NAME
+import matplotlib.pyplot as plt
 import tqdm
 
 
@@ -21,4 +22,49 @@ waymo_base_kwargs = {
 
 waymo_val = WaymoValidationMini(**waymo_base_kwargs)
 
-# /n/fs/pci-sharedt/ij9461/try2/generative-detection/data
+# ERROR: No such file or directory: 'data/waymo/waymo_mini_infos_val.pkl'
+# Right now: data/processed/waymo/....
+#   no pkl file in there
+
+print(len(waymo_val))
+print(waymo_val[5])
+
+# mark patch_center_2d as red on full image
+idx = 50
+
+for idx in tqdm.tqdm(range(len(waymo_val))):
+    full_img = waymo_val[idx].full_img.permute(1,2,0)
+
+    # mark patch_center_2d as red on full image 
+    patch_center_2d = waymo_val[idx].patch_center_2d # torch.Size([2])
+
+idx = 11
+patch = waymo_val[idx].patch.permute(1,2,0).numpy()
+plt.imshow(patch)
+plt.savefig('patch.png')
+plt.show()
+
+full_img = waymo_val[idx].full_img.permute(1,2,0)
+
+# mark patch_center_2d as red on full image 
+patch_center_2d = waymo_val[idx].patch_center_2d # torch.Size([2])
+
+full_img = full_img.numpy()
+full_img = full_img.copy() # (900, 1600, 3)
+
+# make patch_center_2d as red with surrounding pixels red
+
+patch_center_2d = patch_center_2d.numpy()
+patch_center_2d = patch_center_2d.astype(int)
+patch_center_2d = patch_center_2d.tolist()
+
+for i in range(-5, 6):
+    for j in range(-5, 6):
+        full_img[patch_center_2d[1]+i, patch_center_2d[0]+j, :] = [1,0,0]
+        
+plt.imshow(full_img)
+plt.savefig('patch2.png')
+plt.show()
+
+print("Image and Patch Shapes:", nusc_val[0].full_img.shape, nusc_val[0].patch.shape)
+print("MIN AND MAX:", waymo_val[0].full_img.min(), waymo_val[0].full_img.max())
