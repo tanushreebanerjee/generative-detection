b67ec3e120dc84496ce39d207516d57d56e17935
tanushreebanerjee <tb21@princeton.edu>
2024-05-01 23:32:25 -0400

    remove v1, v2, v3, computation

--- dataset_stats.py
+++ dataset_stats.py
@@ -25,39 +25,30 @@ def get_dataset_stats(dataset, save_dir="dataset_stats"):
     t1_meter = AverageMeter()
     t2_meter = AverageMeter()
     t3_meter = AverageMeter()
-    v1_meter = AverageMeter()
-    v2_meter = AverageMeter()
-    v3_meter = AverageMeter()
     l_meter = AverageMeter()
     h_meter = AverageMeter()
     w_meter = AverageMeter()
     pbar = tqdm.tqdm(total=len(nusc_train))
     for i in range(len(dataset)):
         data = nusc_train[i]
-        t1, t2, t3, v1, v2, v3 = data['pose_6d']
+        t1, t2, t3, _, _, _ = data['pose_6d']
         l, h, w = data['bbox_size']
         t1_meter.update(t1)
         t2_meter.update(t2)
         t3_meter.update(t3)
-        v1_meter.update(v1)
-        v2_meter.update(v2)
-        v3_meter.update(v3)
         l_meter.update(l)
         h_meter.update(h)
         w_meter.update(w)
         
         pbar.update(1)
         if i % 100 == 0:
-            pbar.set_description(f"t1: {t1_meter.get_stats()} t2: {t2_meter.get_stats()} t3: {t3_meter.get_stats()} v1: {v1_meter.get_stats()} v2: {v2_meter.get_stats()} v3: {v3_meter.get_stats()} l: {l_meter.get_stats()} h: {h_meter.get_stats()} w: {w_meter.get_stats()}")
+            pbar.set_description(f"t1: {t1_meter.get_stats()} t2: {t2_meter.get_stats()} t3: {t3_meter.get_stats()} l: {l_meter.get_stats()} h: {h_meter.get_stats()} w: {w_meter.get_stats()}")
         
     # save stats to file and print to stdout
     stats = {
         "t1": t1_meter.get_stats(),
         "t2": t2_meter.get_stats(),
         "t3": t3_meter.get_stats(),
-        "v1": v1_meter.get_stats(),
-        "v2": v2_meter.get_stats(),
-        "v3": v3_meter.get_stats(),
         "l": l_meter.get_stats(),
         "h": h_meter.get_stats(),
         "w": w_meter.get_stats(),
@@ -68,6 +59,8 @@ def get_dataset_stats(dataset, save_dir="dataset_stats"):
         print(f"{k}: {v}")
     with open(os.path.join(save_dir, f"{dataset.__class__.__name__}_stats.txt"), "w") as f:
         f.write(str(stats))    
+        
+    return stats
 
 def main():
     nusc_base_kwargs = {
@@ -85,8 +78,8 @@ def main():
     nusc_train = NuScenesTrain(**nusc_base_kwargs)
     nusc_val = NuScenesValidation(**nusc_base_kwargs)
 
-    get_dataset_stats(nusc_train)
-    get_dataset_stats(nusc_val)
+    train_stats = get_dataset_stats(nusc_train)
+    val_stats = get_dataset_stats(nusc_val)
 
 if __name__ == "__main__":
     main()
